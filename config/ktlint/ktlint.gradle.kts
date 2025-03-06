be536d5590b262c76e099f27501d10863268d405
Ruy Mesquita <rmesquito@letsgetchecked.com>
2025-03-06 14:32:10 +0000

    Initial commit, sample project with ktlint

---
+++ config/ktlint/ktlint.gradle.kts
@@ -0,0 +1,32 @@
+configurations {
+  create("ktlint")
+}
+
+dependencies {
+  "ktlint"("com.pinterest.ktlint:ktlint-cli:1.5.0")
+}
+
+tasks.register<JavaExec>("ktlint") {
+  group = "verification"
+  description = "Check Kotlin code style."
+  classpath = configurations.getByName("ktlint")
+  mainClass.set("com.pinterest.ktlint.Main")
+  args(
+    "-F",
+    "src/**/*.kt",
+    "--reporter=plain",
+    "--reporter=checkstyle,output=${buildDir}/reports/ktlint/ktlint-checkstyle.xml"
+  )
+}
+
+tasks.named("check") {
+  dependsOn("ktlint")
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+  group = "formatting"
+  description = "Fix Kotlin code style deviations."
+  classpath = configurations.getByName("ktlint")
+  mainClass.set("com.pinterest.ktlint.Main")
+  args("-F", "src/**/*.kt")
+}
