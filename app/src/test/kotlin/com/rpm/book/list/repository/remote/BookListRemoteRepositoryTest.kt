e8a43866e3fbaf8096b99079cbcde1feac089e25
Ruy Mesquita <rmesquito@letsgetchecked.com>
2025-03-09 11:02:43 +0000

    Minor updates

---
+++ app/src/test/kotlin/com/rpm/book/list/repository/remote/BookListRemoteRepositoryTest.kt
@@ -0,0 +1,4 @@
+package com.rpm.book.list.repository.remote
+
+import org.junit.Assert.*
+ class BookListRemoteRepositoryTest
---
+++ app/src/test/kotlin/com/rpm/book/list/usecases/impl/BookListUseCaseImpTest.kt
@@ -0,0 +1,4 @@
+package com.rpm.book.list.usecases.impl
+
+import org.junit.jupiter.api.Assertions.*
+ class BookListUseCaseImpTest
