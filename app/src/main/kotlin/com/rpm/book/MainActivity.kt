7a1e96b3bd797dd4b64bc6d92061431fc48e904d
Ruy Mesquita <rmesquito@letsgetchecked.com>
2025-03-09 11:17:39 +0000

    new navigation method added

--- app/src/main/kotlin/com/rpm/book/MainActivity.kt
+++ app/src/main/kotlin/com/rpm/book/MainActivity.kt
@@ -1,17 +1,14 @@
-package com.rpm.booklist
+package com.rpm.book
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.rpm.booklist.list.intent.BookListIntent
-import com.rpm.booklist.list.view.BookListScreen
-import com.rpm.booklist.list.viewmodel.BookListViewModel
-import com.rpm.booklist.ui.theme.BookListTheme
+import com.rpm.book.list.viewmodel.BookListViewModel
+import com.rpm.book.navigation.AppScreen
+import com.rpm.book.navigation.appNavGraph
 import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
@@ -19,37 +16,15 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-
       val navController = rememberNavController()
       val bookListViewModel: BookListViewModel by inject()
-//      val bookDetailViewModel: BookDetailViewModel = hiltViewModel()
 
       NavHost(
         navController = navController,
-        startDestination = "list"
+        startDestination = AppScreen.BookList.route,
       ) {
-        composable("list") {
-          BookListTheme {
-            BookListScreen(viewModel = bookListViewModel) {
-              navController.navigate("detail/${it}")
-            }
-          }
-        }
-        composable("detail/{id}") {
-          BookListTheme {
-            Text("Android")
-          }
-        }
-//        appNavGraph(
-//          navController = navController,
-//          bookListViewModel = bookListViewModel,
-//          bookDetailViewModel = bookDetailViewModel
-//        )
+        appNavGraph(navController, bookListViewModel)
       }
-
-//      BookListTheme {
-//        Text("Android")
-//      }
     }
   }
 }
--- app/src/main/kotlin/com/rpm/book/app/BookApplication.kt
+++ app/src/main/kotlin/com/rpm/book/app/BookApplication.kt
@@ -1,15 +1,18 @@
-package com.rpm.booklist.app
+package com.rpm.book.app
 
 import android.app.Application
-import com.rpm.booklist.list.module.bookListModule
+import com.rpm.book.list.module.bookListModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class BookApplication : Application() {
   override fun onCreate() {
     super.onCreate()
 
     startKoin {
+      androidLogger(level = Level.DEBUG)
       androidContext(this@BookApplication)
       modules(bookListModule)
     }
